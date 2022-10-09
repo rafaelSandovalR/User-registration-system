9b10cd731196a2a0c657299a38970d71d7c2acc5
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2022-10-09 14:22:13 -0400

    Initial Commit
    Created project

---
+++ user-registration-demo/user-registration-demo/src/main/java/com/example/userregistrationdemo/UserRegistrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.userregistrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationDemoApplication.class, args);
+	}
+
+}
---
+++ user-registration-demo/user-registration-demo/src/test/java/com/example/userregistrationdemo/UserRegistrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userregistrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
